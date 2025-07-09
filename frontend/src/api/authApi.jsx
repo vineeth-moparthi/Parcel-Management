6455998815612450a1320a5bac5113ded73d1a7a
Abhishek Sunkarwad <abhisunkarwad2002@gmail.com>
2025-07-09 13:00:02 +0530

    backend for user registration

--- frontend/src/api/authApi.jsx
+++ frontend/src/api/authApi.jsx
@@ -5,3 +5,7 @@ const BASE_URL = 'http://localhost:8080/api'; // Spring Boot backend
 export const loginUser = async (credentials) => {
   return axios.post(`${BASE_URL}/login`, credentials);
 };
+
+export const registerUser = async (credentials) => {
+  return axios.post(`${BASE_URL}/registration`, credentials);
+};
